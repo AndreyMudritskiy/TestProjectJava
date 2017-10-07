71924afe183ed969671ef5f1668e80c24d826e54
EFSOL\igor.mudritskiy <i.mudritskiy@efsol.ru>
2017-10-07 19:38:03 +0300

    simple api

---
+++ src/main/java/com/testAPI/Controllers/FoodsController.java
@@ -0,0 +1,18 @@
+package com.testAPI.Controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FoodsController {
+    @RequestMapping(
+            value = "/Foods/Ping",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String Ping()
+    {
+        return "OK";
+    }
+}
