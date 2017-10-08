82be8427985d6443d6ef84130cd0bc497840b530
EFSOL\igor.mudritskiy <i.mudritskiy@efsol.ru>
2017-10-08 18:52:05 +0300

    1. Методы контролера 2. модель 3. Сервис(Autowired не завелся)

--- src/main/java/com/testAPI/Controllers/FoodsController.java
+++ src/main/java/com/testAPI/Controllers/FoodsController.java
@@ -1,18 +1,55 @@
 package com.testAPI.Controllers;
 
+import com.testAPI.Model.Food;
+import com.testAPI.Services.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Foods")
 public class FoodsController {
+
+    //@Autowired
+    private FoodService service = new FoodService();    // Autowired не заводится
+
     @RequestMapping(
-            value = "/Foods/Ping",
+            value = "/Ping",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public String Ping()
     {
         return "OK";
     }
+
+    @RequestMapping(
+            value = "/get",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Food get(String ID)
+    {
+        return service.Get(ID);
+    }
+
+    @RequestMapping(
+            value = "/post",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void post(Food model)
+    {
+        service.Update(model);
+    }
+
+    @RequestMapping(
+            value = "/put",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void put(Food model)
+    {
+        service.Insert(model);
+    }
+
+
 }
---
+++ src/main/java/com/testAPI/Services/FoodService.java
@@ -0,0 +1,21 @@
+package com.testAPI.Services;
+
+import com.testAPI.Model.Food;
+
+public class FoodService
+{
+    public Food Get(String ID)
+    {
+        return new Food();
+    }
+
+    public void Insert(Food model)
+    {
+
+    }
+
+    public void Update(Food model)
+    {
+
+    }
+}
